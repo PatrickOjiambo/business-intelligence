ba0f38fd62fce7b17251744221b6b998ca8d6623
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-04-26 18:40:27 +0300

    feat: DB Schema

---
+++ supermarket_sales/db.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, DateTime
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+class Branch(Base):
+    __tablename__ = 'branch'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Float)
+
+class City(Base):
+    __tablename__ = 'city'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+class Invoice(Base):
+    __tablename__ = 'invoice'
+    id = Column(Integer, primary_key=True)
+    customer_type = Column(String)
+    tax = Column(Float)
+    quantity = Column(Integer)
+    date = Column(DateTime)
+    time = Column(DateTime)
+    payment = Column(String)
+    cost_of_goods = Column(Float)
+    gross_margin_percentage = Column(Float)
+    gross_income = Column(Float)
+    rating = Column(Float)
+
+class FactTable(Base):
+    __tablename__ = 'fact_table'
+    id = Column(Integer, primary_key=True)
+    branch_id = Column(Integer, ForeignKey('branch.id'))
+    product_id = Column(Integer, ForeignKey('product.id'))
+    city_id = Column(Integer, ForeignKey('city.id'))
+    invoice_id = Column(Integer, ForeignKey('invoice.id'))
+
+    branch = relationship('Branch', backref='fact_table')
+    product = relationship('Product', backref='fact_table')
+    city = relationship('City', backref='fact_table')
+    invoice = relationship('Invoice', backref='fact_table')
